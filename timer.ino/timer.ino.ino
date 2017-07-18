ddf6c20ba6aeaef06be28ee7535a758d274ea594
josieesh <eshkenazi.josephine@gmail.com>
2017-07-17 20:50:17 -0400

    fixed timer

--- timer.ino/timer.ino.ino
+++ timer.ino/timer.ino.ino
@@ -1,81 +1,53 @@
-#include <LiquidCrystal.h>
-#include <Arduino.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-
-volatile int ovrflw=0;
+volatile int seconds = 0;
 
 void setup() {
   Serial.begin(9600);
-  //lcd.begin(16,2);
-  //pinMode(2, INPUT);
-  
-  cli(); //disabled timer interrupts
 
-  TCCR1A = 0; //disable the timer while we set it up
+  cli(); //disable interrupts
+
+  TCCR1A = 0;
   TCCR1B = 0;
-  TCCR1B = (5<<0); //set clock frequency to clk/1024
-  TCNT1 = 0; //make sure tnct register starts at 0 when program starts
+  TCCR1B |= (1<<CS12) | (1<<CS10); //set frequency to clk/1024
+  
+  TIMSK1 |= (7<<0); //enables OCR interrupts
+  OCR1A = 15625;
 
-  TIMSK1 |= (7<<0); //enable interrupts of timer1
-  //OCR1A = 65535;
-  sei(); //enable timer interrupts
+  sei(); //enable interrupts
 }
 
-/*ISR(TIMER1_COMPA_vect) {
-  Serial.println("overflow");
-  Serial.println(ovrflw);
-  ovrflw++;
+ISR (TIMER1_COMPA_vect) {
+  Serial.println("Reached 15625");
+  seconds ++;
+  TCNT1 = 0;
 }
-*/
-//overflow fnction to increment the ovrflw global variable so it can be used in the new millis()
-ISR(TIMER1_OVF_vect) {
-  ovrflw++;
-  Serial.print("overflow fnc");
-}
+
 
 void loop() {
-  Serial.println("inside loop");
-  Serial.println(ovrflw);
-
-  //lcd.setCursor(0,0);  
-
-  while(1) {
-    long timerValue = newMillis();
-    TCCR1B = 0; //disable timer1 while printing
-    //lcd.print(timerValue);
-    TCCR1B = B00000101; //reenable timer1 to clock frequency of /1024
-  }
-  
-
+  Serial.println(newMillis());
 }
 
 
-
-long newMillis(){
-
-  Serial.println("inside newMillis");
+double newMillis(){
 
   //disabled interrupts while reading from timer to avoid getting inconsistent values
   cli();
-  long t = TCNT1;
-  Serial.print("t = ");
-  Serial.println(t);
-  long millisec = ((ovrflw*65535) + t)/15.625;
-
-  Serial.print("millis = ");
-  Serial.println(millisec);
+  
+  double t = TCNT1;
+  
+  double millisec = (seconds + t/15625)*1000;
+  //Serial.println(seconds);
+  //Serial.println(millisec);
 
   sei();
 
   return millisec;
 }
 
+
+
 void newDelay(int amt) {
-  long startTime = newMillis();
-  long endTime = newMillis(); 
+  double startTime = newMillis();
+  double endTime = newMillis(); 
   while(endTime - startTime < amt) {
     endTime = newMillis();
   }
